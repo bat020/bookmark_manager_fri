0d1dd196747f44d5c088eac3c0a79691174d4861
philip <philip.clarke@redhood.co.uk>
2015-09-23 08:54:24 +0100

    added failing new link test and erb file for new view

--- app/app.rb
+++ app/app.rb
@@ -3,11 +3,23 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    redirect('/links')
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
+  get '/links/new' do
+    "hello world"
+    erb :'links/new_link'
+  end
+
+  post '/links/new' do
+
+  end
 run! if app_file == $PROGRAM_NAME
 
 end
