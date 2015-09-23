bee65c42c181407e4a746b586c58ac9d92bea2de
philip <philip.clarke@redhood.co.uk>
2015-09-23 16:20:22 +0100

    changed datamapper to auto migrate

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,10 +7,10 @@ require './app/models/link'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'], "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(ENV['DATABASE_URL'], "postgres://localhost/bookmark_manager_#{env}")
 
 
 
 DataMapper.finalize
 
-DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
