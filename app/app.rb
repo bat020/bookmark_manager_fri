2d43e64f0eca6831aba6d5ab4dadadea34439b72
philip <philip.clarke@redhood.co.uk>
2015-09-24 10:01:57 +0100

    added matching password feature test

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,9 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     redirect('/links')
   end
@@ -38,12 +41,18 @@ class BookmarkManager < Sinatra::Base
     erb :'users/new'
   end
 
-  post 'users' do
-    User.create(email: params[:email],
+  post '/users' do
+    user = User.create(email: params[:email],
                 password: params[:password])
+    session[:user_id] = user.id
     redirect to('/links')
   end
 
+  helpers do
+   def current_user
+     @current_user ||= User.get(session[:user_id])
+   end
+  end
 
 run! if app_file == $PROGRAM_NAME
 
